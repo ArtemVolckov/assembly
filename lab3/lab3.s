3b2191adf863729d538a002a2045c0c80fb6bbfd
ArtemVolckov <volckov2004@yandex.ru>
2024-08-04 03:57:50 +0300

    lab3 search first word update

--- lab3/lab3.s
+++ lab3/lab3.s
@@ -1,9 +1,18 @@
 bits 64
-; String handling. Deleting words whose last character does not match the last character of the first word
-; Input - standart input (stdin); Output - file
+;   String handling. Deleting words whose last character does not match the last character of the first word
+;   Input - standart input (stdin); Output - file
 section .data
-    err_msg_1  db  "Can not open the file"
-    err_msg_1_len equ $-err_msg_1
+    err_open db "Can not open the file", 10
+    err_open_len equ $-err_open
+
+    err_read db "Can not read from the stdin", 10
+    err_read_len equ $-err_read
+
+    err_write db "Can not write in the file", 10
+    err_write_len equ $-err_write
+
+    err_close db "Can not close the file", 10
+    err_close_len equ $-err_close
     
 section .bss
     ; Define buffers for storing characters and lines.
@@ -15,7 +24,22 @@ section .bss
     line_buffer resb 64
     line_buffer_size equ $-line_buffer
 
+    ; file descriptor
+    fd  resq  1
+    
+    ; boolean variable if there is a word in the string
+    is_word  resb  1
+
+    ; boolean variable if there is end of a string
+    is_end  resb  1
+
 section .text
+
+%define ERR_OPEN  1
+%define ERR_READ  2
+%define ERR_WRITE 3
+%define ERR_CLOSE 4
+
     global _start
 
 _start:
@@ -28,10 +52,14 @@ _start:
     xor rdx, rdx
     syscall
 
-    ; open error check
+    ; check open error
     test rax, rax
-    js err_msg_1_print
+    js err_open_end
 
+    ; save fd
+    mov qword [fd], rax 
+
+read_line:
     ; read from stdin
     mov rax, 0
     mov rdi, 0
@@ -39,23 +67,100 @@ _start:
     mov rdx, line_buffer_size 
     syscall
     
-    ; write into file
+    ; check read error 
+    test rax, rax
+    js err_read_end
+    
+    ; r8 -> counter
+    xor r8, r8
+    dec r8
+    
+search_first_word:
+    inc r8
+    cmp r8, line_buffer_size
+    je m2
 
+    ; check if space
+    cmp byte [line_buffer+r8], ' '
+    je search_first_word
+
+    ; check if tab
+    cmp byte [line_buffer+r8], 9
+    je search_first_word
+
+    ; check if new line symbol
+    cmp byte [line_buffer+r8], 10
+    je m3
+    
+    mov byte [is_word], 1
+
+m2:
+    cmp byte [is_word], 0
+    je read_line
+
+    jmp end
+ 
+    mov rcx, line_buffer_size
+
+    ; write into file
+    mov rax, 1
+    mov rdi, qword [fd]
+    mov rsi, line_buffer
+    mov rdx, line_buffer_size
+    syscall
+
+    ; check write error 
+    test rax, rax
+    js err_write_end
+
+file_close:
     ; close file
     mov rax, 3
-   
-success:
-    mov edi, 0
-    mov rax, 60
+    mov rdi, qword [fd]
     syscall
-err_msg_1_print:
+
+    ; check close error
+    test rax, rax
+    js err_close_end  
+    
+    mov rdi, 0
+    jmp end
+   
+err_open_end:
     mov rax, 1
     mov rdi, 2
-    mov rsi, err_msg_1
-    mov rdx, err_msg_1_len
-    syscall 
-error:
-    mov rdi, 1
+    mov rsi, err_open
+    mov rdx, err_open_len
+    syscall
+    mov rdi, ERR_OPEN
+    jmp end
+
+err_read_end:
+    mov rax, 1
+    mov rdi, 2
+    mov rsi, err_read
+    mov rdx, err_read_len
+    syscall
+    mov rdi, ERR_READ
+    jmp end
+
+err_write_end:
+    mov rax, 1
+    mov rdi, 2
+    mov rsi, err_write
+    mov rdx, err_write_len
+    syscall
+    mov rdi, ERR_WRITE
+    jmp end
+
+err_close_end:
+    mov rax, 1
+    mov rdi, 2
+    mov rsi, err_close
+    mov rdx, err_close_len
+    syscall
+    mov rdi, ERR_CLOSE
+
+end:
     mov rax, 60
     syscall
-    
--- lab3/test.s
+++ lab3/test.s
@@ -1,23 +1,54 @@
-global _start
- 
+section .bss
+    line_buffer resb 16      ; резервируем 128 байт для буфера
+    line_buffer_size equ 16   ; размер буфера
+    total_length resd 1        ; общее количество прочитанных байт
+
 section .text
+    global _start
+
 _start:
-    ; находим количество символов в строке
-    mov rdi, [rsp+8]    ; получим в RDI адрес пути к файлу, который применялся при вызове
-    xor rax, rax        ; AL равен 0
-    mov rcx, -1         ; помещаем очень большое число
-    repne scasb           ; ищем нулевой байт
-    ; в rdi - адрес следующего за нулем символа
-    mov byte [rdi-1], 10   ; заменяем нулевой байт переводом строки
-    sub rdi, [rsp+8]        ; получаем длину строки включая нулевой символ -  RDI-[rsp+8]           
- 
- 
-    ; вывод на строки на экран
-    mov rdx, rdi
-    mov rdi, 1
-    mov rsi, [rsp+8]
-    mov rax, 1
+    ; Инициализация
+    mov dword [total_length], 0
+
+read_loop:
+    ; Считываем данные
+    mov rax, 0                ; syscall номер для sys_read (0)
+    mov rdi, 0                ; дескриптор файла 0 - stdin
+    mov rsi, line_buffer      ; адрес буфера
+    mov rdx, line_buffer_size  ; размер буфера
     syscall
- 
-    mov rax, 60
+
+    ; Проверяем, сколько байт было прочитано
+    cmp rax, 0                ; проверяем, достигнут ли конец ввода (0 байт)
+    je done                    ; если да, переходим к завершению
+
+    ; Обновляем общее количество прочитанных байт
+    add dword [total_length], eax
+
+    ; Если прочитано меньше, чем размер буфера, добавляем нулевой байт
+    cmp rax, line_buffer_size
+    jl add_null               ; если прочитано меньше, добавляем конец строки
+
+    ; В противном случае продолжаем чтение
+    jmp read_loop
+
+add_null:
+    ; Добавляем нулевой байт в конец считанной строки
+    mov eax, [total_length]   ; общее количество прочитанных байт
+    mov byte [line_buffer + eax], 0  ; добавляем символ конца строки
+
+done:
+    ; Здесь можно использовать line_buffer по вашему усмотрению,
+    ; например, вывести его на экран
+
+    ; Вызов sys_write для вывода на экран
+    mov rax, 1                ; syscall номер для sys_write (1)
+    mov rdi, 1                ; дескриптор файла 1 - stdout
+    mov rsi, line_buffer      ; адрес буфера
+    mov rdx, [total_length]   ; количество байт для записи
+    syscall
+
+    ; Завершение программы
+    mov rax, 60               ; syscall номер для sys_exit
+    xor rdi, rdi              ; код возврата 0
     syscall
